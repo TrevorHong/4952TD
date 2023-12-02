0dac1bf5d9699c02ad53ac7cf4b5b0181d8fe757
TrevorHong <65141699+TrevorHong@users.noreply.github.com>
2023-12-01 22:11:44 -0800

    towerbought can be updated now

--- Models/UserService.cs
+++ Models/UserService.cs
@@ -1,10 +1,15 @@
 public class UserService
 {
+
+    private int? towerBought;
     public string? Username { get; set; }
 
     public int? EnemiesKilled { get; set; }
 
     public int? GoldSpent { get; set; }
 
-    public int? TowerBought { get; set; }
+    public int? TowerBought {
+        get => towerBought ?? 0;
+        set => towerBought = value;
+    }
 }
