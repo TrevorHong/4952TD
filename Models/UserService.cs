1e524580eceaa801249bfb0a3a8ebd904375a7e2
Reynard <loreynard@gmail.com>
2023-12-01 20:11:32 -0800

    Updated and moved UserService

---
+++ Models/UserService.cs
@@ -0,0 +1,10 @@
+public class UserService
+{
+    public string? Username { get; set; }
+
+    public int? EnemiesKilled { get; set; }
+
+    public int? GoldSpent { get; set; }
+
+    public int? TowerBought { get; set; }
+}
